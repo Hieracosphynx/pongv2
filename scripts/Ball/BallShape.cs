878bc914e7b1c934c17c52b6fd250246a2223fca
Miguel Cortez <cortezmiguel05@gmail.com>
2024-05-27 20:39:47 -0700

    Positioning

--- scripts/Ball/BallShape.cs
+++ scripts/Ball/BallShape.cs
@@ -5,7 +5,6 @@ public partial class BallShape : CircleShape
 {
     public override void _Ready()
     {
-        NewPosition = Position;
         Radius = 20;
         CircleColor = Colors.DarkOrange;
     }
---
+++ scripts/Game.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class Game : Node
+{
+    private Area2D SpawnableObstacleArea;
+
+    public override void _Ready()
+    {
+    }
+}
