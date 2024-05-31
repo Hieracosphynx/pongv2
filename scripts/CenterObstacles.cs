55ac2387642ac482883494345d3fda85e93cac12
Miguel Cortez <cortezmiguel05@gmail.com>
2024-05-30 20:46:44 -0700

    Removed dummy code

--- scripts/CenterObstacles.cs
+++ scripts/CenterObstacles.cs
@@ -3,14 +3,4 @@ using System;
 
 public partial class CenterObstacles : Node2D
 {
-    private Tween ExampleTween;
-    private StaticBody2D TopLeftCenter;
-
-    public override void _Ready()
-    {
-        TopLeftCenter = GetNode<StaticBody2D>("TopLeftCenterTriangle");
-        ExampleTween = GetTree().CreateTween();
-        ExampleTween.TweenProperty(TopLeftCenter, "rotation", Math.PI * 2, 10).AsRelative();
-        ExampleTween.SetLoops();
-    }
 }
