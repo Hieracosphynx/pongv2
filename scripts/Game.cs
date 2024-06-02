bfc8620d44b84ee8db6188d545a04bc7619ad98c
Miguel Cortez <cortezmiguel05@gmail.com>
2024-06-02 15:09:38 -0700

    Added docker and nakama

--- scripts/Game.cs
+++ scripts/Game.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Nakama;
 using System;
 
 public partial class Game : Node
@@ -7,5 +8,37 @@ public partial class Game : Node
 
     public override void _Ready()
     {
+        InitialiseServer();
+    }
+
+    private async void InitialiseServer()
+    {
+        var httpAdapter = new GodotHttpAdapter();
+
+        AddChild(httpAdapter);
+
+        const string scheme = "http";
+        const string host = "127.0.0.1";
+        const int port = 7350;
+        const string serverKey = "defaultkey";
+
+        // Pass in the 'http_adapter' as the last argument.
+        var client = new Client(scheme, host, port, serverKey, httpAdapter);
+
+        client.Logger = new GodotLogger("Nakama", GodotLogger.LogLevel.DEBUG);
+
+        ISession session;
+        try {
+            session = await client.AuthenticateDeviceAsync(OS.GetUniqueId(), "TestUser", true); 
+        }
+        catch(ApiResponseException e) {
+            GD.PrintErr(e.ToString());
+            return;
+        }
+
+        var websocketAdapter = new GodotWebSocketAdapter();
+        AddChild(websocketAdapter);
+
+        var socket = Socket.From(client, websocketAdapter);
     }
 }
